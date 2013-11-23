731fc23190404e13da7ad90e51618d3de74d8a22
Balaji V <balajivaikar@gmail.com>
2013-11-23 16:06:06 +0530

    Adding TestGenerics

---
+++ TestProject/src/org/bvr/test/test/TestGenerics.java
@@ -0,0 +1,22 @@
+package org.bvr.test.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TestGenerics<K,V> {
+
+	List<String> listTeam = new ArrayList<String>();
+	Map<K,V> map = new HashMap<K,V>();
+	
+	public void addTeam(String teamName, List<String> players) {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestGenerics<K, V> 
+	}
+
+}
---
+++ TestProject/src/org/bvr/test/test/TestHex.java
@@ -0,0 +1,15 @@
+package org.bvr.test.test;
+
+public class TestHex {
+	public static void main(String[] args) {
+		TestHex hex = new TestHex();
+		hex.printHex();
+	}
+
+	public void printHex() {
+		int a = 0xff;
+		int b = 10;
+		System.out.println("println" + b);
+		System.out.printf("printf 0x%x", b);
+	}
+}
