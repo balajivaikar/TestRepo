b6d8d72cb44ede97e85c93055e8cad94c948d0dd
Balaji V <balajivaikar@gmail.com>
2013-11-28 09:00:38 +0530

    Updating Keyboard Threading Test Classes

---
+++ TestProject/src/org/bvr/test/ThreadTest/jobQueue/BaseClass.java
@@ -0,0 +1,13 @@
+package org.bvr.test.ThreadTest.jobQueue;
+
+public class BaseClass {
+	
+	public void getSomething() {
+		
+	}
+	
+	protected void doSomething() {
+		
+	}
+
+}
---
+++ TestProject/src/org/bvr/test/ThreadTest/jobQueue/JobObserver.java
@@ -0,0 +1,5 @@
+package org.bvr.test.ThreadTest.jobQueue;
+
+public abstract class JobObserver {
+
+}
