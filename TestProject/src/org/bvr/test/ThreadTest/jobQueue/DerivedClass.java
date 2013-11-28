13ddb33d436a9c6ecc9705bf549393d6281eec71
Balaji V <balajivaikar@gmail.com>
2013-11-28 09:04:16 +0530

    tests

---
+++ TestProject/src/org/bvr/test/ThreadTest/jobQueue/DerivedClass.java
@@ -0,0 +1,16 @@
+package org.bvr.test.ThreadTest.jobQueue;
+
+public class DerivedClass extends BaseClass {
+
+//	@Override
+	private void getSomething() {
+		// TODO Auto-generated method stub
+		super.getSomething();
+	}
+
+	@Override
+	public void doSomething() {
+		// TODO Auto-generated method stub
+		super.doSomething();
+	}
+}
